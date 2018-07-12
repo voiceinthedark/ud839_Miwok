6f60c6f2dcfc862c050c51f833ecdabd377a456f
porphiros <darkrisingforce@gmail.com>
2018-07-12 22:19:01 +0300

    change: app now uses a view pager to navigate the activities

---
+++ app/src/main/java/com/example/android/miwok/WordsFragmentStatePagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class WordsFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+
+    private Context mContext;
+    /**
+     * Default constructor accepts a fragment manager
+     * @param fragmentManager
+     */
+    WordsFragmentStatePagerAdapter(Context context, FragmentManager fragmentManager){
+        super(fragmentManager);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return mContext.getString(R.string.category_numbers);
+            case 1:
+                return mContext.getString(R.string.category_family);
+            case 2:
+                return mContext.getString(R.string.category_colors);
+            case 3:
+                return mContext.getString(R.string.category_phrases);
+                default:
+                    return null;
+        }
+    }
+}
